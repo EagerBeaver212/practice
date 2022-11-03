69f9e599a972ac2f87d9ae77deae300c948d181d
Greg C <gregcourt10@gmail.com>
2022-11-03 09:12:02 +0000

    first commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// function myFunction() {
+//   alert(Math.round(Math.random()*100));
+// }
+
+// myFunction();
+
+// function hello(name = 'Chris') {
+//   console.log(`Hello ${name}!`);
+// }
+
+// hello('Ari'); // Hello Ari!
+// hello();      // Hello Chris!
+
+function favoriteAnimal(animal) {
+  return animal + " is my favorite animal!"
+}
+
+console.log(favoriteAnimal('Goat'));
+
+alert(favoriteAnimal("dog"));
